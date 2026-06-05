75e1d88097518532d380979814af0cc7f8c90265
Atharva Nitin Pawar <123879019+Atharva0126@users.noreply.github.com>
2026-06-06 01:48:07 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,19 +2,20 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="Category Analysis Dashboard",
-    page_icon="📊",
+    page_title="Global Semiconductor Industry Dashboard",
     layout="wide"
 )
 
-st.title("📊 Category Analysis Dashboard")
-st.markdown("Interactive Tableau Dashboard")
+st.title("📊 Global Semiconductor Industry Analysis Dashboard")
 
-tableau_url = "https://public.tableau.com/views/Capstone_proj_17806898204630/CategoryAnalysis?:showVizHome=no"
+tableau_url = (
+    "https://public.tableau.com/views/"
+    "Capstone_proj_17806898204630/Main?:showVizHome=no"
+)
 
 components.iframe(
-    tableau_url,
+    src=tableau_url,
+    width=1400,
     height=900,
-    width=1200,
     scrolling=True
 )
