00ea79d7132185ab186436e396432c5e9e150c20
Atharva Nitin Pawar <123879019+Atharva0126@users.noreply.github.com>
2026-06-06 01:58:52 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,46 +1,28 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-# Remove Streamlit's default padding
+# Inject a full-page iframe that breaks out of Streamlit's container
 st.markdown("""
     <style>
-        .block-container {
-            padding-bottom: 0rem;
-            padding-left: 0rem;
-            padding-right: 0rem;
-        }
-        .stApp {
-            margin: 0;
-        }
-        iframe {
-            display: block;
+        /* Hide all Streamlit UI */
+        #root > div:first-child { visibility: hidden; }
+        .stApp { background: transparent; }
+
+        /* Full screen overlay iframe */
+        .fullscreen-frame {
+            position: fixed;
+            top: 0; left: 0;
+            width: 100vw;
+            height: 100vh;
+            z-index: 9999;
+            border: none;
         }
     </style>
-""", unsafe_allow_html=True)
 
-html_code = """
-<style>
-    * { margin: 0; padding: 0; box-sizing: border-box; }
-    body { overflow: hidden; }
-    .tableau-container {
-        width: 100vw;
-        height: 100vh;
-    }
-    iframe {
-        width: 100%;
-        height: 100%;
-        border: none;
-        display: block;
-    }
-</style>
-<div class='tableau-container'>
     <iframe
-        src='https://public.tableau.com/views/Capstone_proj_17806898204630/Main?:showVizHome=no&:embed=true&:toolbar=no'
+        class="fullscreen-frame"
+        src="https://public.tableau.com/views/Capstone_proj_17806898204630/Main?:showVizHome=no&:embed=true&:toolbar=yes&:fullscreen=true"
         allowfullscreen>
     </iframe>
-</div>
-"""
-
-components.html(html_code, height=950, scrolling=False)
+""", unsafe_allow_html=True)
