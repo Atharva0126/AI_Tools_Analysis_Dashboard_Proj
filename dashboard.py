90078c068156183ed79fe23132aea669a92e65cc
Atharva Nitin Pawar <123879019+Atharva0126@users.noreply.github.com>
2026-06-06 01:52:58 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,47 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="auto")
+st.set_page_config(layout="wide")
+
+# Remove Streamlit's default padding
+st.markdown("""
+    <style>
+        .block-container {
+            padding-top: 0rem;
+            padding-bottom: 0rem;
+            padding-left: 0rem;
+            padding-right: 0rem;
+        }
+        .stApp {
+            margin: 0;
+        }
+        iframe {
+            display: block;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 html_code = """
-<div class='tableauPlaceholder' id='viz'>
+<style>
+    * { margin: 0; padding: 0; box-sizing: border-box; }
+    body { overflow: hidden; }
+    .tableau-container {
+        width: 100vw;
+        height: 100vh;
+    }
+    iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+        display: block;
+    }
+</style>
+<div class='tableau-container'>
     <iframe
-        src='https://public.tableau.com/views/Capstone_proj_17806898204630/Main?:showVizHome=no'
-        width='100%'
-        height='900'
-        frameborder='0'>
+        src='https://public.tableau.com/views/Capstone_proj_17806898204630/Main?:showVizHome=no&:embed=true&:toolbar=no'
+        allowfullscreen>
     </iframe>
 </div>
 """
 
-components.html(html_code, height=950)
+components.html(html_code, height=950, scrolling=False)
