c1cd7d5ec8a94c37177ab365a8cb6753b5cbb4ad
Atharva Nitin Pawar <123879019+Atharva0126@users.noreply.github.com>
2026-06-06 01:48:44 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,21 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(
-    page_title="Global Semiconductor Industry Dashboard",
-    layout="wide"
-)
+st.set_page_config(layout="wide")
 
-st.title("📊 Global Semiconductor Industry Analysis Dashboard")
+html_code = """
+<div class='tableauPlaceholder' id='viz'>
+    <iframe
+        src='https://public.tableau.com/views/Capstone_proj_17806898204630/Main?:showVizHome=no'
+        width='100%'
+        height='900'
+        frameborder='0'>
+    </iframe>
+</div>
+"""
 
-tableau_url = (
-    "https://public.tableau.com/views/"
-    "Capstone_proj_17806898204630/Main?:showVizHome=no"
-)
-
-components.iframe(
-    src=tableau_url,
-    width=1400,
-    height=900,
-    scrolling=True
-)
+components.html(html_code, height=950)
